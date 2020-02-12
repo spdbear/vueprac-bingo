f8ea7fe0bb6c4f5e323dad513800be3510fadd66
spdbear <16793732+spdbear@users.noreply.github.com>
2020-02-12 17:56:19 +0900

    引いた番号のリセットを実装

--- main.js
+++ main.js
@@ -17,41 +17,54 @@ var app = new Vue({
         num = this.selectedNumbers[this.selectedNumbers.length - 1];
       }
       return num;
+    },
+    drawTimes: function () {
+      return 75 - this.unselectedNumbers.length;
     }
   },
 
   // ライフサイクルハック
   // mounted: DOM構築直後
   mounted: function () {
-    // 配列をシャッフル（破壊的変更）
-    function shuffleArray(array) {
-      for (var i = array.length - 1; i > 0; i--) {
-        var r = Math.floor(Math.random() * (i + 1));
-        var tmp = array[i];
-        array[i] = array[r];
-        array[r] = tmp;
-      }
-      return array;
-    }
-    for (var i = 1; i <= 75; i++) {
-      this.unselectedNumbers.push(i);
-    }
-    shuffleArray(this.unselectedNumbers);
     this.loadNumbers();
   },
 
   // このアプリケーションで使うメソッド
   methods: {
+    init() {
+      // 配列をシャッフル（破壊的変更）
+      function shuffleArray(array) {
+        for (var i = array.length - 1; i > 0; i--) {
+          var r = Math.floor(Math.random() * (i + 1));
+          var tmp = array[i];
+          array[i] = array[r];
+          array[r] = tmp;
+        }
+        return array;
+      }
+      this.selectedNumbers.length = 0;
+      this.unselectedNumbers.length = 0;
+      for (var i = 1; i <= 75; i++) {
+        this.unselectedNumbers.push(i);
+      }
+      shuffleArray(this.unselectedNumbers);
+    },
+
     draw() {
       if (this.unselectedNumbers.length != 0) {
         this.selectedNumbers.push(this.unselectedNumbers.pop());
         this.saveNumbers();
       }
     },
-    // TODO: 番号を引いた履歴をリセットできるようにする
-    reset() {
 
+    // 番号を引いた履歴をリセット
+    restart() {
+      if (window.confirm("Are you sure you want to restart?")) {
+        this.init();
+        this.saveNumbers();
+      }
     },
+
     loadNumbers() {
       const myStorage = localStorage;
       if (myStorage.getItem("BingoMachineStorage")) {
@@ -60,10 +73,15 @@ var app = new Vue({
           this.selectedNumbers = loadData.selected;
           this.unselectedNumbers = loadData.unselected;
         } catch (e) {
-          myStorage.removeItem("BingoMachineStorage")
+          const loadData = {
+            "selected": this.selectedNumbers,
+            "unselected": this.unselectedNumbers
+          };
+          myStorage.setItem("BingoMachineStorage", JSON.stringify(loadData));
         }
       }
     },
+
     saveNumbers() {
       const myStorage = localStorage;
       const saveData = {
